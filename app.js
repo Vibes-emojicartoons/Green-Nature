7e63cb022d866e147cf5a68fac92a18f62144882
Vibes-emojicartoons <143393526+Vibes-emojicartoons@users.noreply.github.com>
2024-02-13 08:03:16 +0200

    last changes

---
+++ app.js
@@ -0,0 +1,44 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list')
+let thumbnailDom = document.querySelector('.carousel .thumbnail')
+
+nextDom.click = function(){
+    showSlider('next');
+}
+function showSlider(type){
+    let itemSlider = document.querySelector('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item')
+}
+prevDom.oneclick = function(){
+    showSlider('prev')
+}
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun  = setTimeout(() =>{
+    nextDom.click();
+}, timeAutoNext);
+
+
+if(type === 'next'){
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.aa('next');
+}else{
+    let positionLastItem = itemSlider.length -1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add('prev');
+}
+clearTimeout(runTimeOut);
+runTimeOut = setTimeout(() =>{
+    carouselDom.classList.remove('next');
+    carouselDom.classList.remove('prev');
+}, timeRunning);
+
+clearTimeout(runautoRun);
+runAutoRun = setTimeout(() =>{
+    nextDom.click();
+}, timeAutoNext);
